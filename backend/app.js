1c10a8651a388fa92fa9a77171b240580cee924f
tarakoughalim <147817179+tarakoughalim@users.noreply.github.com>
2024-02-03 10:19:06 +0100

    commit

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,24 @@ const Route   = require('../backend/routes/route');
 const router = require('./routes/route');
 const app = express();
 const cors = require('cors');
+const env = require('dotenv').config();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
+const corsOption ={
 
-const dbURI = 'mongodb+srv://tarakoughalim0:12331233@cluster0.ti6lbqt.mongodb.net/'
+    origin: process.env.FRONTEND_URL,
+}
+app.use(cors(corsOption));
+console.log(process.env.FRONTEND_URL);
+
+
+const dbURI = process.env.DB_URL
+console.log(dbURI)
 mongoose.connect(dbURI)
 .then(() =>{
-    app.listen(4000)
+    app.listen(4800)
     console.log('connected to db');
 })
 .catch((err) => console.log(err));
